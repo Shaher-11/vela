c9ca038abe29c84a8247b8309aacd5b9f8fa38b2
Shaher-11 <shahershamroukh@gmail.com>
2022-07-05 21:31:10 +0200

    Add validations and annotate models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: products
+#
+#  id                :bigint           not null, primary key
+#  title             :string
+#  short_description :string
+#  description       :text
+#  original_price    :decimal(8, 2)
+#  final_price       :decimal(8, 2)
+#  avatar            :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 class Product < ApplicationRecord
-end
+  validates :title, presence: true
+  validates :short_description , presence: true
+  validates :description , presence: true
+  validates :original_price , presence: true
+  validates :final_price , presence: true
+end 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: products
+#
+#  id                :bigint           not null, primary key
+#  title             :string
+#  short_description :string
+#  description       :text
+#  original_price    :decimal(8, 2)
+#  final_price       :decimal(8, 2)
+#  avatar            :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
