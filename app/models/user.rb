e39d9ec35759388873afd6ae6daa1e087938f455
Shaher-11 <shahershamroukh@gmail.com>
2022-07-06 11:32:41 +0200

    Style products page

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def user_name
+    email[/^[^@]+/].capitalize
+  end
 end
